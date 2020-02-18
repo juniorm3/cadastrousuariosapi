c31372e38542e2818e3fd000b0dd0f2ee1eaaa28
Mario <juniorm3@msn.com>
2020-02-17 23:23:17 -0300

    Criando classes de resources

---
+++ src/main/java/com/jrm3/cadastrousuarios/resources/UsuariosResources.java
@@ -0,0 +1,10 @@
+package com.jrm3.cadastrousuarios.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuariosResources {
+
+}
