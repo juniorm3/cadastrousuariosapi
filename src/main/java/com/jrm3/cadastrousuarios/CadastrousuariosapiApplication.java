b4572d63f87bda629a1b49d5054bd4627d9e81b9
Mario <juniorm3@msn.com>
2020-02-17 21:30:24 -0300

    Criando projeto para gerenciamento de usuarios

---
+++ src/main/java/com/jrm3/cadastrousuarios/CadastrousuariosapiApplication.java
@@ -0,0 +1,13 @@
+package com.jrm3.cadastrousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrousuariosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrousuariosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrm3/cadastrousuarios/CadastrousuariosapiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jrm3.cadastrousuarios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CadastrousuariosapiApplication.class)
+@WebAppConfiguration
+class CadastrousuariosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
