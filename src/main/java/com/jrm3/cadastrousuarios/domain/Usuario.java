3593704641b2143b2264f7b8d81203a566f6a7ab
Mario <mario.martins@sonda.com>
2020-02-24 14:06:57 -0300

    Merge para corrigir problemas de branch

--- src/main/java/com/jrm3/cadastrousuarios/domain/Usuario.java
+++ src/main/java/com/jrm3/cadastrousuarios/domain/Usuario.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
